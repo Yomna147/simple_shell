9c158ac981763c082774ffd73c10f62586c3de5f
Yomna147 <syomna59@gmail.com>
2023-05-21 05:50:48 -0700

    shell

--- shelltry1/main.c
+++ shelltry1/main.c
@@ -2,17 +2,26 @@
 
 int main(int ac, char **argv){
   char *prompt = "(shelltry1) $ ";
-  char *lineptr;
+  char *lineptr = NULL, *lineptr_copy = NULL;
   size_t n = 0; 
+ ssize_t nchars_read;
+ const char *delim = " \n"
 
   /* declaring void variables */
   (void)ac; (void)argv;
+while (8){
+	printf("%s", prompt);
+      nchars_read = getline(&lineptr, &n, stdin);
 
-  printf("%s", prompt);
-  getline(&lineptr, &n, stdin);
+        if (nchars_read == -1){
+            printf("Exiting shell..\n");
+            return (-1);
+        }
+
   printf("%s\n", lineptr);
 
   free(lineptr);
+}
   return (0);
 }
 
